36eb65e8bfc471916fbcdbf5de0798f171a4108f
kplnhsyn <kplnhsyn.49@gmail.com>
2024-02-25 14:22:33 +0100

    feat:AppRouter.jsx added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-import Register from "./pages/Register";
+import AppRouter from "./router/AppRouter";
+import AuthContextProvider from "./context/AuthContext";
+import { ToastContainer } from "react-toastify";
+import MovieContextProvider from "./context/MovieContext";
 
 const App = () => {
     return (
-        <div>
-            <Register />
+        <div className=" dark:bg-gray-dark-main min-h-screen">
+            <AuthContextProvider>
+                <MovieContextProvider>
+                    <AppRouter />
+                    <ToastContainer />
+                </MovieContextProvider>
+            </AuthContextProvider>
         </div>
     );
 };

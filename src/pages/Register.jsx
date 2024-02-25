ee6285b2376f147f465455d50e19f5b3ff6c146d
kplnhsyn <kplnhsyn.49@gmail.com>
2024-02-25 20:05:58 +0100

    feat:Register.jsx updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,4 @@
-// import React, { useContext, useState } from "react";
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import GoogleIcon from "../assets/icons/GoogleIcon";
 import { AuthContext } from "../context/AuthContext";
 
